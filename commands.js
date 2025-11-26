21bff2e5a000da031ec958a8a57a84b71ee51d12
Zapta <zapta@users.noreply.github.com>
2025-11-25 21:21:43 -0800

    Set the apio get examples to overwrite the examples file if it already exists.

--- commands.js
+++ commands.js
@@ -102,7 +102,7 @@ const TOOLS_TREE = [
         id: "apio.new.project",
         action: {
           cmds: [
-            `apio api get-examples -o ${utils.apioTmpFile("examples.json")}`,
+            `apio api get-examples -f -o ${utils.apioTmpFile("examples.json")}`,
           ],
           cmdId: "apio.newProjectWizard",
         },
