5a32575282843b544deefef12f2542048b319271
Zapta <zapta@users.noreply.github.com>
2025-12-28 09:47:57 -0800

    Changed the node scripts to use version 22.

--- scripts/init-test-env.sh
+++ scripts/init-test-env.sh
@@ -1,9 +1,17 @@
-#!/bin/bash  -x
+#!/bin/bash
 
 # Run this once to setup the test env. Otherwise tests will not work.
 
 # Exit on error.
 set -e
+
+# Load nvm (standard location)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Enforce Node.js 22 (install if missing, then switch)
+nvm install 22
+nvm use 22
 
 ./scripts/clean.sh
 
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Exit on error.
 set -e
