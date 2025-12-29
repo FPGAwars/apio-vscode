88964aa09dd90e40331d1cc6db75fae0fa7ae430
Zapta <zapta@users.noreply.github.com>
2025-12-29 13:18:34 -0800

    Added -x to debug a bash script on github workflow.

--- scripts/init-test-env.sh
+++ scripts/init-test-env.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Run this once to setup the test env. Otherwise tests will not work.
 
 # Exit on error.
 set -e
 
+# Avoid a conflict when running under github.
+unset npm_config_prefix
+
 # Load nvm (standard location)
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Avoid a conflict when running under github.
-unset npm_config_prefix
 
 # Enforce Node.js 22 (install if missing, then switch)
 nvm install 22
