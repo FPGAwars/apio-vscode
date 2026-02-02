7928aaef37e2b5ed82c099bc3bb9e7f6ee947c81
Zapta <zapta@users.noreply.github.com>
2026-02-02 14:16:50 -0800

    Refactored the context commands logic to a new file.

---
+++ src/context-cmds.js
@@ -0,0 +1,83 @@
+// Handle the context commands. This are commands that are
+// launched from right-click menus.
+
+// Standard imports
+const vscode = require("vscode");
+
+// Local imports.
+const apioLog = require("./apio-log.js");
+const actions = require("./actions.js");
+
+// A callback for handling commands invocations from vscode context
+// right click menu.
+async function contextCmdHandler(taskTitle, taskCmds, contextUri) {
+  let targetUri;
+
+  apioLog.msg("fileContextHandler() invoked");
+  apioLog.msg(`contextUri=${contextUri}`);
+
+  // Determine the target URI.
+  if (contextUri instanceof vscode.Uri) {
+    // We got the uri from the explorer.
+    targetUri = contextUri;
+  } else {
+    // Otherwise, the invocation from the editor editor.
+    const activeEditor = vscode.window.activeTextEditor;
+    if (activeEditor) {
+      targetUri = activeEditor.document.uri;
+    }
+  }
+
+  // Error if we didn't figure out the.
+  if (!targetUri) {
+    vscode.window.showWarningMessage("No file selected or no active editor.");
+    return;
+  }
+
+  // Convert the uri to a path relative to the workspace.
+  const contextPath = vscode.workspace.asRelativePath(targetUri);
+
+  // Expand the {context-path} placeholder in the task title and
+  // in the commands.
+  taskTitle = taskTitle.replace("{context-path}", contextPath);
+  taskCmds = taskCmds.map((cmd) => cmd.replace("{context-path}", contextPath));
+
+  // Launch the commands as a task.
+  launcher = actions.getActionLauncher(
+    (taskTitle = taskTitle),
+    (taskCmds = taskCmds),
+    (taskCompletionMsgs = null),
+    (urlToOpen = null),
+    (cmdIdToInvoke = null),
+  );
+
+  await launcher();
+}
+
+// Register the handlers for the file context operations
+function registerFileContextHandlers(context, preCmds) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("apio.testContext", (contextUri) =>
+      contextCmdHandler(
+        "CONTEXT / TEST",
+        [...preCmds, `{apio-bin} test "{context-path}" {env-flag}`],
+        contextUri,
+      ),
+    ),
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("apio.simContext", (contextUri) =>
+      contextCmdHandler(
+        "CONTEXT / SIM",
+        [...preCmds, `{apio-bin} sim "{context-path}" {env-flag}`],
+        contextUri,
+      ),
+    ),
+  );
+}
+
+// Export for require()
+module.exports = {
+  registerFileContextHandlers,
+};
--- src/main.js
+++ src/main.js
@@ -33,6 +33,7 @@ const wizard = require("./get-example-wizard.js");
 const tasks = require("./tasks.js");
 const actions = require("./actions.js");
 const envSelector = require("./env-selector.js");
+const contextCmds = require("./context-cmds.js");
 
 // // Place holder for the default apio env.
 // const APIO_ENV_DEFAULT = "(default)";
@@ -330,75 +331,6 @@ function _registerTreeView(context, tree, title, preCmds, viewId) {
   context.subscriptions.push(viewContainer);
 }
 
-// A callback for handling commands invocations from vscode context
-// right click menu.
-async function contextCmdHandler(taskTitle, taskCmds, contextUri) {
-  let targetUri;
-
-  apioLog.msg("fileContextHandler() invoked");
-  apioLog.msg(`contextUri=${contextUri}`);
-
-  // Determine the target URI.
-  if (contextUri instanceof vscode.Uri) {
-    // We got the uri from the explorer.
-    targetUri = contextUri;
-  } else {
-    // Otherwise, the invocation from the editor editor.
-    const activeEditor = vscode.window.activeTextEditor;
-    if (activeEditor) {
-      targetUri = activeEditor.document.uri;
-    }
-  }
-
-  // Error if we didn't figure out the.
-  if (!targetUri) {
-    vscode.window.showWarningMessage("No file selected or no active editor.");
-    return;
-  }
-
-  // Convert the uri to a path relative to the workspace.
-  const contextPath = vscode.workspace.asRelativePath(targetUri);
-
-  // Expand the {context-path} placeholder in the task title and
-  // in the commands.
-  taskTitle = taskTitle.replace("{context-path}", contextPath);
-  taskCmds = taskCmds.map((cmd) => cmd.replace("{context-path}", contextPath));
-
-  // Launch the commands as a task.
-  launcher = actions.getActionLauncher(
-    (taskTitle = taskTitle),
-    (taskCmds = taskCmds),
-    (taskCompletionMsgs = null),
-    (urlToOpen = null),
-    (cmdIdToInvoke = null),
-  );
-
-  await launcher();
-}
-
-// Register the handlers for the file context operations
-function _registerFileContextHandlers(context, preCmds) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("apio.testContext", (contextUri) =>
-      contextCmdHandler(
-        "CONTEXT / TEST",
-        [...preCmds, `{apio-bin} test "{context-path}" {env-flag}`],
-        contextUri,
-      ),
-    ),
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand("apio.simContext", (contextUri) =>
-      contextCmdHandler(
-        "CONTEXT / SIM",
-        [...preCmds, `{apio-bin} sim "{context-path}" {env-flag}`],
-        contextUri,
-      ),
-    ),
-  );
-}
-
 // Performs the dynamic configurations of the extension such as
 // showing or hiding views and buttons. Calls once from activate()
 // and then each time apio.ini changes.
@@ -611,7 +543,7 @@ function activate(context) {
   envSelector.registerApioEnvSelector(context, wsInfo, 90);
 
   // Register the file context commands handlers.
-  _registerFileContextHandlers(context, preCmds);
+  contextCmds.registerFileContextHandlers(context, preCmds);
 
   // Perform the dynamic configuration. This function is called
   // again latter each time apio.ini changes.
