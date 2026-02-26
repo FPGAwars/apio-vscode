7685844a185a5f7388ea653b416d27bf220f25b3
Zapta <zapta@users.noreply.github.com>
2026-02-26 12:32:57 -0800

    Reworked the shell initialization for windows CMD vs PS1.

--- src/shell-cmd.js
+++ src/shell-cmd.js
@@ -1,6 +1,8 @@
 // Handles the apio-shell command.
 
 const vscode = require("vscode");
+const path = require("path");
+const fs = require("fs");
 
 // Local imports
 const downloader = require("./downloader.js");
@@ -18,27 +20,55 @@ const apioLog = require("./apio-log.js");
 function registerApioShellCommand(context, wsInfo) {
   const TERMINAL_NAME = "Apio Shell";
 
-  // Construct the shell startup commands.
-  let preCmds = [];
-  {
-    if (platforms.isWindows()) {
-      // For windows (CMD and Powershell)
-      preCmds.push("cls");
-      preCmds.push(`set "PATH=${utils.apioBinDir()};%PATH%"`);
-      if (wsInfo.wsDirPath) preCmds.push(`cd "${wsInfo.wsDirPath}"`);
-      preCmds.push("apio -h");
-    } else {
-      // For macOS and Linux (bash)
-      preCmds.push("printf '\\ec'"); // cls
-      preCmds.push(`export PATH="${utils.apioBinDir()}:$PATH"`);
-      if (wsInfo.wsDirPath) preCmds.push(`cd "${wsInfo.wsDirPath}"`);
-      preCmds.push("apio -h");
-    }
-    apioLog.msg(`Shell preCmds: ${preCmds}`);
-  }
+  // Used for windows only.
+  const cmdFilePath = path.join(utils.apioTmpDir(), "shell-init.cmd");
+  const ps1FilePath = path.join(utils.apioTmpDir(), "shell-init.ps1");
 
   const disposable = vscode.commands.registerCommand("apio.shell", async () => {
-    // 1. Dispose ALL terminals named "Apio"
+    // if windows, create the shell initialization script for CMD and PS1.
+    if (platforms.isWindows()) {
+      const cmdLines = [
+        ":: Automatically generated initialization VSCode 'apio shell'.",
+        "@echo off",
+        `echo Initializing for CMD`,
+        `echo set "PATH=${utils.apioBinDir()};%%PATH%%"`,
+        `set "PATH=${utils.apioBinDir()};%PATH%"`,
+      ];
+      utils.writeFileFromLines(cmdFilePath, cmdLines);
+
+      const ps1Lines = [
+        "# Automatically generated initialization for VSCode 'apio shell'",
+        `Write-Host "Initializing for PowerShell"`,
+        `Write-Host '$env:PATH = "${utils.apioBinDir()};$env:PATH"'`,
+        `$env:PATH = "${utils.apioBinDir()};$env:PATH"`,
+      ];
+      utils.writeFileFromLines(ps1FilePath, ps1Lines);
+    }
+
+
+    // Construct commands to send to the terminal.
+    wsDirExists = wsInfo.wsDirPath && fs.existsSync(wsInfo.wsDirPath);
+
+    let preCmds = [];
+    {
+      if (platforms.isWindows()) {
+        // For windows, CMD and PS1.
+        preCmds.push("cls");
+        // This select the cmd or ps1 startup init file.
+        preCmds.push(`call "${cmdFilePath}" || . "${ps1FilePath}"`);
+        if (wsDirExists) preCmds.push(`cd "${wsInfo.wsDirPath}"`);
+        preCmds.push("apio -h");
+      } else {
+        // For macOS and Linux (bash)
+        preCmds.push("printf '\\ec'"); // cls
+        preCmds.push(`export PATH="${utils.apioBinDir()}:$PATH"`);
+        if (wsDirExists) preCmds.push(`cd "${wsInfo.wsDirPath}"`);
+        preCmds.push("apio -h");
+      }
+      apioLog.msg(`Shell preCmds: ${preCmds}`);
+    }
+
+    // Dispose ALL terminals named "Apio"
     const apioTerminals = vscode.window.terminals.filter(
       (t) => t.name === TERMINAL_NAME,
     );
@@ -53,7 +83,12 @@ function registerApioShellCommand(context, wsInfo) {
     }
 
     // Create a brand-new terminal
-    const terminal = vscode.window.createTerminal({ name: TERMINAL_NAME });
+    const terminal = vscode.window.createTerminal({
+      name: TERMINAL_NAME,
+      // This is just a safe initial cwd before .bashrc and shell and
+      // preCmds overrides.
+      cwd: utils.userHomeDir(),
+    });
 
     // Make the terminal visible.
     terminal.show();
